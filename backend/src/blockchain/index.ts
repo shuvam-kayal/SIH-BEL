88fa3c08117d2009bc302fdc65e6b141f569b9a0
SriVineel <srivineel39@gmail.com>
2026-09-19 23:34:54 +0530

    feat: export Besu committee source

--- backend/src/blockchain/index.ts
+++ backend/src/blockchain/index.ts
@@ -16,4 +16,5 @@ export {
   type SubmitResult,
 } from "./evm-adapter";
 export { createBlockchainServiceFromEnv } from "./factory";
+export { BesuConsensusSource, type BelCommitteeResponse } from "./besu-consensus-source";
 export { assertWalletMatchesPublicKey, parseCompactSignature, publicKeyToEvmAddress, verifyCompactSignature } from "./crypto";
